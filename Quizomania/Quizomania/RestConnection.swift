b323e34bde726f168130d53905c52c992a3d4724
Mariusz Czeszejko-Sochacki <s396309@pcd2p.labs.wmi.amu.edu.pl>
2016-05-23 12:09:04 +0200

    first commit

---
+++ Quizomania/Quizomania/RestConnection.swift
@@ -0,0 +1,14 @@
+//
+//  RestConnection.swift
+//  Quizomania
+//
+//  Created by Jakub Janicki on 18.05.2016.
+//  Copyright © 2016 Mariusz Czeszejko-Sochacki. All rights reserved.
+//
+
+import Foundation
+
+
+class RestConncetion: NSObject {
+
+}
